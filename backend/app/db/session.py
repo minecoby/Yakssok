b65375a2e4ad35d6d2fd9a9ed7bd07a159b32d06
minecoby <127065808+minecoby@users.noreply.github.com>
2025-09-18 16:21:12 +0900

    feat: 구글 로그인 구현 (#3)
    ## 📝작업 내용
    
    > 전체적인 구글로그인 기능구현입니다. 이후 추가로 유효토큰검사와 리프레시토큰재발급 기능을 구현할 예정입니다.

---
+++ backend/app/db/session.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from app.variable import *
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL_USER,pool_recycle=3600, pool_pre_ping=True)
+
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
---
+++ backend/app/variable.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL_USER = os.environ.get("SQLALCHEMY_DATABASE_URL_USER")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
+REFRESH_TOKEN_EXPIRE_MINUTES = int(os.environ.get("REFRESH_TOKEN_EXPIRE_MINUTES"))
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+FRONTEND_URL = os.getenv("FRONTEND_URL","localhost:5173")
+
